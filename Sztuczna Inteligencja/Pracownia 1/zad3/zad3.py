7026007ed9d906e050cec8ac36d18fd27b4ab41e
8molik <blejzoo@gmail.com>
2025-03-07 16:23:19 +0100

    add cards

--- Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
+++ Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
@@ -1,13 +1,26 @@
+import random
+
 # colors = ['spade', 'club', 'diamond', 'heart']
 # figures = ['ace', 'jack', 'king', 'queen']
 
 colors = ['pik', 'trefl', 'karo', 'kier']
-figures = ['as', 'jopek', 'krol', 'dama']
-numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
+numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jopek', 'dama', 'krol', 'as']
 
-figurant_cards = [(colors[i], figures[j]) for i in range(len(colors)) for j in range(len(figures))]
-blotkarz_cards = [(colors[i], numbers[j]) for i in range(len(colors)) for j in range(len(numbers))]
- 
-print(figurant_cards)
-print(blotkarz_cards)
+figurant_cards = [(numbers[j], colors[i]) for i in range(len(colors)) for j in range(9)]
+blotkarz_cards = [(numbers[j], colors[i]) for i in range(len(colors)) for j in range(9, len(numbers))]
 
+figurant_hand = random.sample(figurant_cards, 5)
+blotkarz_hand = random.sample(blotkarz_cards, 5)
+
+print(figurant_hand)
+print(blotkarz_hand)
+
+def calculate_hand_score(hand, owner='blotkarz'):
+    hand = sorted(hand, key=lambda x: numbers.index(x[0]), reverse=True)
+    
+
+
+    print(hand)
+
+calculate_hand_score(blotkarz_hand)
+calculate_hand_score(figurant_hand, 'figurant')
