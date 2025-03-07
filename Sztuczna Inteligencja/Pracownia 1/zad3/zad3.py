a65e84b5193ad641f22a43541e36b4783aebd132
8molik <blejzoo@gmail.com>
2025-03-07 15:34:02 +0100

    add cards

--- Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
+++ Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
@@ -1,7 +1,13 @@
-colors = ['spade', 'club', 'diamond', 'heart']
-figures = ['ace', 'jack', 'king', 'queen']
+# colors = ['spade', 'club', 'diamond', 'heart']
+# figures = ['ace', 'jack', 'king', 'queen']
 
+colors = ['pik', 'trefl', 'karo', 'kier']
+figures = ['as', 'jopek', 'krol', 'dama']
+numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-figurant_cards = [(color[i], figures[j]) for i in range(len(colors) for j in range(len)figures)]
-
+figurant_cards = [(colors[i], figures[j]) for i in range(len(colors)) for j in range(len(figures))]
+blotkarz_cards = [(colors[i], numbers[j]) for i in range(len(colors)) for j in range(len(numbers))]
+ 
 print(figurant_cards)
+print(blotkarz_cards)
+
