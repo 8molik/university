6fbfb4ce44faabaa9dacc2a34cc1eaec1390ec3a
8molik <blejzoo@gmail.com>
2025-03-09 21:32:38 +0100

    fix poker rules

--- Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
+++ Sztuczna Inteligencja/Pracownia 1/zad3/zad3.py
@@ -13,18 +13,12 @@ blotkarz_cards = [(numbers[j], colors[i]) for i in range(len(colors)) for j in r
 figurant_hand = random.sample(figurant_cards, 5)
 blotkarz_hand = random.sample(blotkarz_cards, 5)
 
-
-
+# Simplified rules
 def calculate_hand_score(hand):
     hand = sorted(hand, key=lambda x: numbers.index(x[0]), reverse=True)
-    highest_card = hand[0]
-
-    # Check if cards are straight
     card_counts = Counter(card[0] for card in hand)
     card_count_proportions = sorted([card_counts[i] for i in card_counts], reverse=True)
-    print(card_count_proportions)
-    print(card_counts)
-    print(hand)
+
     is_straight = True
     same_color = True
     for i in range(1, len(hand)):
@@ -32,45 +26,35 @@ def calculate_hand_score(hand):
             is_straight = False
         if hand[i-1][1] != hand[i][1]:
             same_color = False
-
-    print(is_straight)
-    highest_card = hand[0][0]
-    # 9 is the highest score
     if is_straight and same_color:
-        return 9, highest_card
+        return 9
     if card_count_proportions == [4, 1]:
-        # tutaj trzeba porównać oba rodzaje kart, wygrywa ten kto ma większy
-        # dla uproszczenia wybieram najwyższy
-        return 8, highest_card
+        return 8
     if card_count_proportions == [3, 2]:
-        return 7, highest_card
+        return 7
     if card_count_proportions == [1, 1, 1, 1, 1] and same_color:
-        return 6, highest_card
+        return 6
     if is_straight and not same_color: 
-        # nie rozpatruję przypadku A 2 3 4 5
-        return 5, highest_card
+        return 5
     if card_count_proportions == [3, 1, 1]:
-        # trzeba dodać przypadek silniejszej trójki
-        return 4, highest_card
+        return 4
     if card_count_proportions == [2, 2, 1]:
-        # trzeba dodać moc dwójek
-        return 3, highest_card
-    
-
-
-
-    print(hand)
-
-
-def compare_strength(hand1, hand2):
-    s1 = calculate_hand_score(hand1)
-    s2 = calculate_hand_score(hand2)
-    
-    if s1 < s2:
-        return s2
-    elif s1 > s2:
-        return s1
+        return 3
+    if card_count_proportions == [2, 1, 1, 1]:
+        return 2
     else:
-        return s1 if hand1[0] > hand2[0] else s2    
+        return 1
+
+# We ignore rule of the strongest card, since 
+# figurant cards are always stronger, than blotkarz cards
+def compare_strength(blotkarz, figurant):
+    s1 = calculate_hand_score(blotkarz)
+    s2 = calculate_hand_score(figurant)
+    return 'blotkarz' if s1 > s2 else 'figurant'
 
 calculate_hand_score(figurant_hand)
+calculate_hand_score(blotkarz_hand)
+print(figurant_hand)
+print(blotkarz_hand)
+
+print(compare_strength(figurant_hand, blotkarz_hand))
