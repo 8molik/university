31d99da40b1bd38ec7e39649828566d259530150
8molik <blejzoo@gmail.com>
2025-03-22 18:47:48 +0100

    networks: fix duplicated output

--- Sieci  Komputerowe/traceroute.cpp
+++ Sieci  Komputerowe/traceroute.cpp
@@ -53,11 +53,16 @@ void send_echo_request(int ttl, int sequence) {
                                  (struct sockaddr*)&dest_addr, sizeof(dest_addr));
 
     if (bytes_sent < 0) {
-        std::cerr << "Error sending packet" << std::endl;
+        std::cerr << "send_echo_request: Error while sending packet!" << std::endl;
     }
 }
 
-int receive_response() {
+struct ResponsePacket {
+    std::string ip;
+    int time;
+};
+
+std::vector<ResponsePacket> receive_response() {
     auto start_time = std::chrono::high_resolution_clock::now();
     int received_responses = 0;
 
@@ -65,17 +70,17 @@ int receive_response() {
     ps.fd = socket_fd;
     ps.events = POLLIN;
     ps.revents = 0;
+
+    std::vector<ResponsePacket> responses;
     
     while (received_responses < NUMBER_OF_PACKETS) {
-        
         int remaining_time = RECV_TIMEOUT_MS - std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
         
         if (remaining_time <= 0) {
             break;
         }
 
-        int ready = poll (&ps, 1, RECV_TIMEOUT_MS);
-        std::cout << ready << std::endl;
+        int ready = poll(&ps, 1, RECV_TIMEOUT_MS);
 
         if (ready < 0) {
             std::cerr << "Error in poll" << std::endl;
@@ -85,6 +90,7 @@ int receive_response() {
         }
 
         if (ps.revents == POLLIN) {
+            ResponsePacket response;
             uint8_t buffer[IP_MAXPACKET];
             struct sockaddr_in recv_addr;
             socklen_t recv_addr_len = sizeof(recv_addr);
@@ -93,18 +99,20 @@ int receive_response() {
                                             (struct sockaddr*)&recv_addr, &recv_addr_len);
                                             
             if (bytes_received < 0) {
-                std::cerr << "Error receiving packet" << std::endl;
+                std::cerr << "receive_response: Error while receiving packet!" << std::endl;
             } else {
                 received_responses++;
-                std::cout << "Received packet from " << inet_ntoa(recv_addr.sin_addr) << std::endl;
+                response.ip = inet_ntoa(recv_addr.sin_addr);
+                response.time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
             }
+            responses.push_back(response);
         }
     }
-    return received_responses;
+    return responses;
 }
 
 int main() {
-
+    std::string destination_ip = "8.8.8.8";
     if (socket_fd < 0) {
         std::cerr << "Error while creating socket" << std::endl;
         return 1;
@@ -112,7 +120,7 @@ int main() {
     memset(&dest_addr, 0, sizeof(dest_addr));
 
     dest_addr.sin_family = AF_INET;
-    dest_addr.sin_addr.s_addr = inet_addr("8.8.8.8");
+    dest_addr.sin_addr.s_addr = inet_addr(destination_ip.c_str());
 
     for (int ttl = 1; ttl <= MAX_TTL; ttl++) {
         if (setsockopt(socket_fd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
@@ -123,12 +131,16 @@ int main() {
         for (int i = 1; i <= NUMBER_OF_PACKETS; i++) {
             send_echo_request(ttl, i);
         }
-        int responses = receive_response();
+        
+        auto responses = receive_response();
 
-        if (responses == 0) {
+        if (responses.size() == 0) {
             std::cout << ttl << "  * * *  Request timed out." << std::endl;
-        } else {
-            std::cout << ttl << "  Received " << responses << " response(s)" << std::endl;
+        } else { 
+            std::cout << responses[0].time << "  " << responses[0].ip << std::endl;
+            if (responses[0].ip == destination_ip) {
+                return 0;
+            }
         }
     }
 
