5eecbc89667934c945becad94df12a285514f9a5
8molik <blejzoo@gmail.com>
2025-04-01 14:05:45 +0200

    frontend: add exmaple usage, fix functions

--- Kurs Frontendu/List 5/main.js
+++ Kurs Frontendu/List 5/main.js
@@ -57,11 +57,18 @@ const editQuantity = (id, newQuantity) => {
 
 const editDate = (id, newDate) => {
     const product = productsList.find(product => product.id === id);
-    if (product) {
+    const date = new Date(newDate);
+
+    const isValidDate = (date) => {
+        return date instanceof Date && !isNaN(date.getTime());
+    };
+
+    if (product && isValidDate(date)) {
         product.date = newDate;
         return true;
+    } else {
+        throw "Unsupported data format! (YYYY-MM-DD)";
     }
-    return false;
 }
 
 const editStatus = (id, newStatus) => {
@@ -87,6 +94,74 @@ const swapProducts = (id1, id2) => {
 
 const toBePaidToday = () => {
     const today = new Date();
-    return productsList.filter(product => product.status && product.date.toDateString() === today.toDateString());
+    return productsList.filter(product => !product.status && product.date === today.toISOString().split('T')[0]);
 }
 
+const addPriceToBoughtProduct = (id, price) => {
+    const product = productsList.find(product => product.id === id);
+    if (product.status) {
+        product.price = price;
+    } else {
+        return false;
+    }
+    return true;
+}
+
+const basketPriceAtSpecificDay = (date) => {
+    const totalPrice = productsList.reduce((acc, product) => {
+        if (product.status && product.date === date) {
+            return acc + (product.price || 0);
+        }
+        return acc
+    }, 0);
+    return totalPrice;
+}
+
+const applyFunctionToProducts = (ids, tFunction) => {
+    productsList.forEach(product => {
+        if (ids.includes(product.id)) {
+            tFunction(product)
+        }
+    })
+}
+
+
+const testProduct1 = addProduct("Milk", 2, "2025-04-01", false);
+const testProduct2 = addProduct("Bread", 1, "2025-04-01", true, 3.5);
+const testProduct3 = addProduct("Butter", 3, "2025-03-01", true);
+const testProduct4 = addProduct("Chicken", 1, "2025-04-01", true, 10);
+const testProduct5 = addProduct("Beef", 1, "2025-04-01", true, 10);
+
+console.log("Initial products:", productsList);
+
+editName(testProduct1.id, "Oat Milk");
+editQuantity(testProduct1.id, 27);
+editStatus(testProduct1.id, true);
+editDate(testProduct1.id, "2025-04-15")
+removeProduct(testProduct5.id);
+
+console.log("\n================\n")
+console.log("After editing and removal:", productsList);
+console.log("\n================\n")
+
+
+swapProducts(testProduct1.id, testProduct3.id)
+console.log("\n================\n")
+console.log("After swapping:", productsList);
+console.log("\n================\n")
+
+console.log("Products to buy today:", toBePaidToday());
+
+
+addPriceToBoughtProduct(testProduct4.id, 111)
+addPriceToBoughtProduct(testProduct3.id, 111)
+console.log("\n================\n")
+console.log("After swapping:", productsList);
+console.log("\n================\n")
+
+
+console.log(basketPriceAtSpecificDay("2025-04-01"))
+
+applyFunctionToProducts([testProduct1.id, testProduct2.id], (product) => {product.quantity += 1})
+
+console.log(productsList)
